588f0b33be5479833c5a434e850baa692e3501e2
Lekha Ladhe <ladhelekha@gmail.com>
2022-08-01 17:41:20 +0530

    initialized

---
+++ if-else code.py
@@ -0,0 +1,16 @@
+n = 10
+if n % 2 == 0 and 2 <= n <= 5:
+    print(" Not Wierd")
+    
+elif n % 2 == 0 and 6 <= n <= 10:
+    print("Weird")
+    
+elif n % 2 == 0 and n > 20:
+    print("Not weird")
+    
+else:
+    print("not weird")
+    
+
+    
+    
---
+++ loops.py
@@ -0,0 +1,7 @@
+n = int(input())
+for i in range(0,21):
+    if i < n:
+        print(i**2)
+    
+    
+    
